8963577bb26ec752533d24527bd8f8f89214d494
Lori Schlatter <lori.schlatter@gmail.com>
2020-07-20 19:57:47 -0500

    Completed Mod 1 assignments with passing tests.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,20 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for i, val in enumerate(arr):
+        if val == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
+    high = len(arr) - 1
+    low = 0
+    while high >= low:
+        mid = (high + low) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            low = mid + 1
     return -1  # not found
